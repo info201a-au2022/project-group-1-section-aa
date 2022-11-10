8e4aac60343e18391cad722a1981da87a5562f5f
OliviaSapp2 <114948288+OliviaSapp2@users.noreply.github.com>
2022-11-10 15:29:55 -0800

    Update Spotify_2022.R

--- data/Spotify_2022.R
+++ data/Spotify_2022.R
@@ -2,21 +2,29 @@ library("dplyr")
 library("stringr")
 library("tidyverse")
 
+# at the moment I am still trying to figure out how to put the data set into the GitHub
+# in the meantime you can follow these steps to get the data set and filter it down
+# to one data set that we can all use
+ 
+# go to https://www.kaggle.com/datasets/jfreyberg/spotify-chart-data?resource=download
+# download the data set, I named mine 'Spotify tracks data.csv'
+
+# load the .csv into R studio
+#read.csv("the name of the .csv you downloaded from kaggle")
 spotiify_origional <- read.csv("Spotify tracks data.csv")
 
+# filters down the data
+# removes the track id, explicit, and duration columns
 spotify_modify <- spotiify_origional %>% 
   select(name, country, date, position, streams, artists, genres = artist_genres)
 
+#returns all the data just from 2022
+#this is the data set you should you on the project
 spotify_2022 <- spotify_modify %>% 
   filter(date >= "2022-01-01") %>% 
   arrange(date) %>% 
   group_by(date)
-  
-  
-spotify_position <- spotify_2022%>% 
-  group_by(date) %>% 
-  arrange(position) 
-  
-spotify_recent <- spotify_modify %>% 
-  filter(date == max(date))
+
+
+save(spotify_2022, file = "spotify_tracks_2022")
 
