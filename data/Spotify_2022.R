a7a32da565264c130848df4ca55e2d258d56a372
Jiajie He <114967277+jiajih16@users.noreply.github.com>
2022-11-14 01:17:17 -0800

    Update Spotify_2022.R

--- data/Spotify_2022.R
+++ data/Spotify_2022.R
@@ -5,6 +5,7 @@ library("scales")
 library("ggplot2")
 install.packages("maps")
 library("maps")
+library(scales)
 
 # I wanted to make our data set smaller so it could be easier to use
 # The full dataset is called charts.csv and the one I made is called spotify_2022.csv
@@ -155,3 +156,38 @@ ggplot(top_country.map, aes(map_id = region, fill = yes_no))+
   scale_fill_viridis_c(option = "C") +
   ggtitle("Was the Global Top Song of 2022 (Pepas) Popular in these Countries?")
 
+#pie chart which summaries the most popular genres across the globe(top 10)
+coutries_genres <- spotiify_origional %>% 
+  select(country, genres = artist_genres) %>% 
+  separate(genres, c("genres"), sep = ',')
+View(coutries_genres)
+
+coutries_genres$genres<-gsub("]","",as.character(coutries_genres$genres))
+View(coutries_genres)
+
+num_genres <- count(coutries_genres, genres)
+num_genres <- num_genres[-c(1), ]
+num_genres$n <- as.numeric(as.character(num_genres$n)) / 1000
+
+View(num_genres)
+
+top_10 <- num_genres[order(num_genres$n, decreasing = TRUE), ]
+top_10 <- top_10[1:10, ]
+top_10 <- top_10 %>% 
+  mutate(percentage = n/sum(n))
+View(top_10) 
+
+# Create a bar
+pie <- ggplot(top_10, aes(x="", y=n, fill=genres)) + geom_bar(stat="identity", width=1)
+# Convert to pie by adding polar coordinates, then add labels
+pie <- pie + coord_polar("y", start=0) + geom_text(aes(label = paste0(round(percentage*100), "%")), position = position_stack(vjust = 0.5))
+# Add color scales
+pie <- pie + scale_fill_manual(values=c("#55DDE0", "#33658A", "#2F4858", "#F6AE2D", "#F26419", "#F8766D", "yellow", "red", "blue", "green")) 
+# Remove labels and add title
+pie<-pie + labs(x = NULL, y = NULL, fill = "Genres", title = "Most Popoular genres across the globe (in thousand)")
+# reorganiza the theme
+pie<-pie + theme_classic() + theme(axis.line = element_blank(),
+                                    axis.text = element_blank(),
+                                    axis.ticks = element_blank(),
+                                    plot.title = element_text(hjust = 0.5, color = "#666666"))
+pie
