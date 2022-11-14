9928a12f9e603bc0d7ff311050cce5178b5d493c
OliviaSapp2 <114948288+OliviaSapp2@users.noreply.github.com>
2022-11-13 18:14:33 -0800

    Update Spotify_2022.R

--- data/Spotify_2022.R
+++ data/Spotify_2022.R
@@ -48,3 +48,13 @@ col_chart <- ggplot(data = top_3) +
   ggtitle("Top song's daily streamings across the globe") +
   theme(plot.title = element_text(hjust = 0.5))
 col_chart
+
+top_100 <- spotify_2022_global[order(spotify_2022_global$streams, decreasing = TRUE), ]
+top_100 <- top_100[1:100, ]
+View(top_100)  
+top_100$streams <- as.numeric(top_100$streams)
+
+scatter_chart <- ggplot(data = top_100) +
+  geom_point(mapping = aes(x = position, y = streams))
+scatter_chart <- scatter_chart + scale_y_continuous(limits = c(985000, 1000000)) 
+scatter_chart
